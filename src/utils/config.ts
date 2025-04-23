b3b6aaffc59874394225418c767e6feda36511f9
Himank Jain <himankjain@yahoo.in>
2025-04-23 16:29:44 +0530

    commented code delete

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -4,11 +4,5 @@ import { parseEther } from "viem";
 export async function loadConfig(path: string = "config.toml") {
     const configFile = Bun.file(path);
     let config = parse(await configFile.text());
-
-    // config.faucet.networks.eth = config.faucet.networks.eth.map((ele: any) => {
-    //     ele.threshold = parseEther(ele.threshold)
-    //     return ele
-    // })
-
     return config
 }
